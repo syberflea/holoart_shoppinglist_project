a98541cef0edb7eda5150ea00de91d74aa7a9b69
syberflea <syberlea@gmail.com>
2024-01-26 14:25:17 +0300

    Init SQLite DB

--- haloart/haloart/settings.py
+++ haloart/haloart/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'secret')
 
-DEBUG = os.getenv('DEBUG', 'False')
+# DEBUG = os.getenv('DEBUG', 'True')
+DEBUG = True
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split()
 
